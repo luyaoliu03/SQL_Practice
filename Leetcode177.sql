ba7ffa9b43a2f4fb51a5a443c64615a3371c8e0c
luyaoliu03 <Luyao.liu03@gmail.com>
2020-05-05 15:33:11 -0400

    Update Leetcode177.sql

--- Leetcode177.sql
+++ Leetcode177.sql
@@ -1,1 +1,18 @@
+/* To get Nth highest salary, create a function */
 
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+DECLARE M INT;
+SET M = N-1;
+  RETURN (
+      IFNULL(
+      (SELECT DISTINCT Salary
+      FROM Employee
+      ORDER BY Salary Desc
+      LIMIT M,1), NULL)
+  );
+END
+
+/* Why we have to re-create M as (N-1)?
+Bcause MySQL only takes numeric constants in the LIMIT syntax.
+*/
