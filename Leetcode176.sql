d2c5e364307ada95825737140c4a6ea9f0cc2664
luyaoliu03 <Luyao.liu03@gmail.com>
2020-05-05 11:46:31 -0400

    Update Leetcode176.sql

--- Leetcode176.sql
+++ Leetcode176.sql
@@ -1,4 +1,20 @@
-/* It's not correct - need to be fixed */
+/* IFNULL function */
+
+SELECT IFNULL(
+(SELECT DISTINCT SALARY
+FROM Employee
+ORDER BY Salary Desc
+LIMIT 1,1), NULL)
+AS SecondHighestSalary
+;
+
+/*LIMIT (offset A, row_counts B)
+SKIP A rows, start to count from A+1, the Bth row
+Here LIMIT 1,1 means the second row
+https://www.mysqltutorial.org/mysql-limit.aspx
+*/
+
+/* Why this can't work
 
 SELECT CASE WHEN
 (SELECT SALARY
@@ -8,3 +24,5 @@ LIMIT 1,1) IS NULL
 THEN NULL
 END AS SecondHighestSalary
 ;
+
+*/
