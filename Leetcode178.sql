5692dd65720f0547d83ada6abb47989a95512646
luyaoliu03 <Luyao.liu03@gmail.com>
2020-12-25 14:31:19 -0500

    Update Leetcode178.sql

--- Leetcode178.sql
+++ Leetcode178.sql
@@ -1,1 +1,10 @@
+SELECT score
+     , DENSE_RANK() OVER (ORDER BY score DESC) AS 'rank'
+FROM scores
+ORDER BY 2 ASC
+;
 
+/*
+mySQL needs quote for the column name
+DENSE_RANK() doesn't skip the numbers, and will give the same number for the same rank.
+*/
